0352e8cdb6bea661cbcc738ad5c9fca35b7e8462
Luck Voltia <vluck7183@gmail.com>
2024-06-06 13:56:03 +0545

    first commit

---
+++ WebApplication3/About.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Jack" AutoEventWireup="True" Inherits="WebApplication3.About" CodeBehind="~/About.aspx.cs" Language="C#" %>
+
+<form runat="server">
+    <asp:Label ID="id" runat="server" Text="id"></asp:Label>
+    <asp:TextBox ID="txtId" runat="server"></asp:TextBox>
+    <asp:Label ID="user" runat="server" Text="User"></asp:Label>
+    <asp:TextBox ID="txtUser" runat="server"></asp:TextBox>
+    <asp:Label ID="pass" runat="server" Text="Password"></asp:Label>
+
+    <asp:TextBox ID="txtPass" runat="server"></asp:TextBox>
+    <asp:Button ID="Insert" runat="server" OnClick="btnLogin_Click" Text="Insert"></asp:Button>
+    <asp:Button ID="Update" runat="server" OnClick="UpdateData" Text="Update"></asp:Button>
+    <asp:Button ID="Delete" runat="server" OnClick="DeleteData" Text="Delete"></asp:Button>
+
+
+    <asp:TextBox ID="txtSearch" runat="server"></asp:TextBox>
+    <asp:Button ID="Button1" runat="server" OnClick="btnSearch_Click" Text="Search"></asp:Button>
+    <asp:GridView ID="gridView" runat="server"></asp:GridView>
+
+
+    <asp:RequiredFieldValidator runat="server" ControlToValidate="txtUser" ErrorMessage="Please Enter User"></asp:RequiredFieldValidator>
+        <asp:RequiredFieldValidator runat="server" ControlToValidate="txtPass" ErrorMessage="Please Enter Password"></asp:RequiredFieldValidator>
+    <asp:RegularExpressionValidator runat="server" ControlToValidate="txtUser" ErrorMessage="Please Enter Valid User Name" ValidationExpression="^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$"></asp:RegularExpressionValidator>
+
+
+
+
+</form>
---
+++ WebApplication3/About.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+
+
+namespace WebApplication3
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ShowData();
+
+            }
+        }
+
+
+        public void btnLogin_Click(object obj, EventArgs args)
+        {
+            try
+            {
+                string ab = "server=localhost;Database=loggin;User ID=root;Password=;";
+                using (MySqlConnection con = new MySqlConnection(ab))
+                {
+                    con.Open();
+                    string query = "Insert into loggin(user, pass) values(@name, @pass)";
+                    using (MySqlCommand command = new MySqlCommand(query, con))
+                    {
+                        command.Parameters.AddWithValue("@name", txtUser.Text);
+                        command.Parameters.AddWithValue("@pass", txtPass.Text);
+
+                        command.ExecuteNonQuery();
+                        ShowData();
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        public void ShowData()
+        {
+            string ab = "server=localhost;Database=loggin;User ID=root;Password=;";
+            using (MySqlConnection con = new MySqlConnection(ab))
+            {
+                con.Open();
+                string query = "select * from loggin";
+                using (MySqlCommand command = new MySqlCommand(query, con))
+                {
+                    DataTable dataTable = new DataTable();
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    gridView.DataSource = dataTable;
+                    gridView.DataBind();
+                }
+            }
+        }
+
+        public void UpdateData(object obj, EventArgs evg)
+        {
+            string ab = "server=localhost;Database=loggin;User ID=root;Password=;";
+            using (MySqlConnection con = new MySqlConnection(ab))
+            {
+                con.Open();
+                string query = "Update loggin Set user = @name, pass = @pass where id = @id";
+                using (MySqlCommand command = new MySqlCommand(query, con))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@name", txtUser.Text);
+                    command.Parameters.AddWithValue("@pass", txtPass.Text);
+
+                    command.ExecuteNonQuery();
+                    ShowData();
+                }
+            }
+        }
+
+        public void DeleteData(object obj, EventArgs args)
+        {
+            string ab = "server=localhost;Database=loggin;User ID=root;Password=;";
+            using (MySqlConnection con = new MySqlConnection(ab))
+            {
+                con.Open();
+                string query = "Delete from loggin where id = @id";
+                using (MySqlCommand command = new MySqlCommand(query, con))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.ExecuteNonQuery();
+                    ShowData();
+                }
+            }
+        }
+
+        public void btnSearch_Click(object obj, EventArgs args)
+        {
+            string ab = "server=localhost;Database=loggin;User ID=root;Password=;";
+            using (MySqlConnection con = new MySqlConnection(ab))
+            {
+                con.Open();
+                string query = "select * from loggin where user like @name";
+                using (MySqlCommand command = new MySqlCommand(query, con))
+                {
+                    command.Parameters.AddWithValue("@name", "%" + txtSearch.Text + "%");
+                    DataTable dataTable = new DataTable();
+                    MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+                    dataAdapter.Fill(dataTable);
+                    gridView.DataSource = dataTable;
+                    gridView.DataBind();
+                }
+            }
+        }
+
+    }
+}
